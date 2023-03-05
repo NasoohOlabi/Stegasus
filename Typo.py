1da5c2fe80bdb706e447754d73e3998fa5d5b5c2
Nasooh Olabi <nasooholabi@gmail.com>
2023-03-05 13:23:14 +0300

    v2

--- Typo.py
+++ Typo.py
@@ -3,30 +3,73 @@
 
 import json
 import math
+import os
 import re
+# Get the path of the script
+from dataclasses import dataclass, field
+from typing import List, Optional, Tuple
 
 import nltk
+import spacy
 from nltk.tokenize import word_tokenize
 
-from .common import *  # pylint: disable=import-error
+# nltk.download('punkt')
 
-nltk.download('punkt')
 
-from dataclasses import dataclass, field
-from typing import List, Tuple
-
-import contextualSpellCheck
-import spacy
-
-SPELL_CHECKER = spacy.load('en_core_web_sm')
-contextualSpellCheck.add_to_pipe(SPELL_CHECKER)
-import os
 
 # Get the path of the script
+
 script_path = os.path.abspath(__file__)
 
-# Construct the path to the file relative to the package
 ROOT_DIR = os.path.dirname(script_path)
+
+
+
+#@title parseRules
+def parseRules(name,ROOT_DIR=ROOT_DIR):
+   with open(ROOT_DIR + f'/{name}.rules','r') as f:
+      lines = f.readlines()
+      cnt = -1
+      results = []
+      for line in lines:
+         cnt+=1
+         line = line[:-1].strip()
+         if len(line) == 0:
+            continue
+         line = line.split('\t')
+         if len(line) > 1:
+            results.append((line[0],line[1]))
+   return results
+
+def trimmed(y,z):
+   return (y[6:]+z[3:],z[:3]+y[6:-1])
+   
+WORD_RULES = parseRules('variant') + parseRules('grammatical') + parseRules('misspelling')
+WORD_RULES = ((x[1:-1],y) for x,y in WORD_RULES)
+SAFE_KEYBOARD_RULES = parseRules('keyboard.safer')
+KEYBOARD_RULES = parseRules('keyboard')
+WORD_CORRECTION_RULES = (trimmed(y,z) for y,z in WORD_RULES)
+def compile_first(x):
+   try:
+      return (re.compile(x[0]),x[1])
+   except:
+      print(x)
+WORD_RULES = list(map(compile_first , WORD_RULES))
+WORD_CORRECTION_RULES = list(map(compile_first , WORD_CORRECTION_RULES))
+KEYBOARD_RULES = list(map(compile_first, KEYBOARD_RULES))
+SAFE_KEYBOARD_RULES = list(map(compile_first, SAFE_KEYBOARD_RULES))
+
+def parse_trigrams(ROOT_DIR=ROOT_DIR):
+   d = dict()
+   with open(ROOT_DIR + '/statistical-attack/english-trigrams.txt', 'r') as f:
+      lines = f.readlines()
+      for line in lines:
+         sp = line.split(' ')
+         if len(sp) > 1:
+            d[sp[0]] = int(sp[1][:-1])
+   return d
+
+PARSED_TRIGRAMS = parse_trigrams()
 
 
 class util:
@@ -65,7 +108,7 @@ class util:
 
 
    @staticmethod
-   def keyboard_rules_scan(text: str, safe=False):
+   def keyboard_rules_scan(text: str, safe=False)->List[Tuple[Tuple[int, int], str, re.Pattern]]:
       matches = []
       rules = SAFE_KEYBOARD_RULES if safe else KEYBOARD_RULES
       for regex, repl in rules:
@@ -76,7 +119,7 @@ class util:
 
 
    @staticmethod
-   def word_rules_scan(text: str):
+   def word_rules_scan(text: str)->List[Tuple[Tuple[int, int], str, re.Pattern]]:
       text_words = util.get_words(text)
       matches = []
       for i, word in enumerate(text_words):
@@ -91,7 +134,7 @@ class util:
       return matches
 
    @staticmethod
-   def rules_scan(text: str):
+   def rules_scan(text: str)-> List[Tuple[Tuple[int, int], str, re.Pattern]]:
       result = util.word_rules_scan(text) + util.keyboard_rules_scan(text)
       result.sort()
       return result
@@ -105,45 +148,91 @@ class util:
       return result
 
    @staticmethod
-   def MatchValidator(text:str,freq=PARSED_TRIGRAMS, verbose=False):
+   def valid_matches(text:str,slots:List[Tuple[Tuple[int, int], str, re.Pattern]],freq=PARSED_TRIGRAMS, verbose=False):
+      """A match is valid if it modifies one word and doesn't modify the first and last letter and doesn't modify the word to a correct spelling word and is decodable"""
+      fist_check = util.spellcheck(text) 
+      if fist_check != text:
+         raise ValueError(f"MatchValidator: text already contains typos '{text}' can't operate on texts with a typo ... the typo is {fist_check}")
       uText = text.upper()
-      uText_words = util.get_words(uText.lower())
+      uText_words = util.get_words(uText)
       valid_regex = re.compile('[A-Z][A-Z][A-Z]')
-      def helper(match_result):
+      mutations: List[str] = list(map(lambda x: '',slots))
+      for match_index, match_result in enumerate(slots):
          span,repl,regex = match_result
-         newString = uText[span[0]-1] + regex.sub(repl,uText[span[0]:span[1]]) + (uText[span[1]] if span[1] < len(uText) else '')
-         tri = filter(lambda x : valid_regex.match(x) is not   None ,util.Trigram(newString))
-         for t in tri:
-            if freq[t] < 10000:
-               if verbose:
-                  print(f"MatchValidator: trigram {t} appears less than 10000 times in corpus")
-               return False
-         newString = util.apply_match(text,  match_result,verbose).lower()
+         
+         newString = util.apply_match(text,  match_result,verbose)
+         mutations[match_index] = newString
+         
+
          words = util.get_words(newString)
          if len(words) != len(uText_words):
             if verbose:
                print(f"MatchValidator: length of words in transformed string ({len(words)}) does not match original string ({len(uText_words)})")
-            return False
+            mutations[match_index] = ''
+            continue
          
          for ow,nw in zip(uText_words,words):
+            ow = ow.lower()
+            nw = nw.lower()
             if ow[0] != nw[0] or ow[-1] != nw[-1]:
                if verbose:
                   print(f"MatchValidator: first and/or last character of word in transformed string ({nw}) does not match original string ({ow})")
-               return False
-         return True
-      return helper
+               mutations[match_index] = ''
+               continue
+
+      if verbose:
+         print(list(zip(slots,mutations)))
+      ambiguous_invalid_matches = []
+
+      for i , newString in enumerate(mutations):
+         if len(newString) == 0:
+            ambiguous_invalid_matches.append(i)
+         else:
+            for j in range(i):
+               if mutations[j] == mutations[i]:
+                  ambiguous_invalid_matches.append(i)
+
+      valid_slots = [elem for i, elem in enumerate(slots) if i not in ambiguous_invalid_matches]
+
+      return valid_slots
 
    @staticmethod
    def valid_rules_scan(text:str,verbose=False):
-      pred = util.MatchValidator(text,verbose)
-      return list(filter(pred,util.rules_scan(text)))
+      proposed_slots = util.rules_scan(text)
+      try:
+         return util.valid_matches(text,proposed_slots,verbose=verbose)
+      except ValueError:
+         return []
+      
+   @staticmethod
+   def chunker(text:str,span_size = 6):
+      # Iterate over the spans in the sentence
+      words = util.get_words(text)
+      chunks = []
+      cur_word = 0
+      last_sep = 0
+      i = 0
+      while i < len(text):
+         if text[i:i+len(words[cur_word])] == words[cur_word]:
+            if cur_word % span_size == 0 and cur_word != 0:
+               chunks.append((last_sep,i))
+               last_sep = i
+            i = i + len(words[cur_word]) - 1
+            cur_word+=1
+         
+         i += 1
+      if last_sep < len(text):
+         chunks.append((last_sep, len(text)))
+      return chunks
+
    @staticmethod
    def spellcheck(text:str,verbose=False):
-      doc = SPELL_CHECKER(text)
-      if verbose:
-         print(f"util.spellcheck('{text}') = '{json.dumps(doc)}'")
-      return doc._.outcome_spellCheck
+      for regex,repl in WORD_CORRECTION_RULES:
+         text =  regex.sub(repl, text)
 
+      if verbose:
+         print(f"util.spellcheck('{text}') = '{text}'")
+      return text
 
 @dataclass
 class Typo:
@@ -151,11 +240,9 @@ class Typo:
 
    text: str = field(repr=False)
    _length: int = field(init=False, repr=False)
-   _slots: List[Tuple[Tuple[int, int], str, re.Pattern]]|None = field(init=True, repr=False,default=None)
-   verbose: bool = False
-
-   def spell_check(self):
-      return util.spellcheck(self.text)
+   _slots: Optional[List[Tuple[Tuple[int, int], str, re.Pattern]]] = field(init=True, repr=False,default=None)
+   _spaces: Optional[List[int]] = field(init=True, repr=False,default=None)
+   verbose: bool = field(init=True,repr=False,default=False)
 
    def __post_init__(self):
       if self.text != util.spellcheck(self.text):
@@ -188,14 +275,23 @@ class Typo:
 
    @property
    def spaces(self) -> List[int]:
-      index_length = self.length
-      s1 = index_length
-      answer = []
-      while s1 > 0 and index_length > 0:
-         s1 = 2 ** math.floor(math.log2(index_length))
-         index_length -= s1
-         answer.append(s1)
-      return answer
+      if self._spaces is not None:
+         return self._spaces
+      
+      sentence_ranges = util.chunker(self.text)
+      
+      # Initialize an empty list of buckets
+      num_buckets = len(sentence_ranges)
+      buckets: List[int] = [0 for _ in range(num_buckets)]
+      
+      # Iterate through each element range and put it in the corresponding bucket
+      for i, (start, end) in enumerate(span for span,_,_ in self.slots):
+         for j, (sent_start, sent_end) in enumerate(sentence_ranges):
+               if sent_start <= start < sent_end and sent_start < end <= sent_end:
+                  buckets[j] += 1
+                  break
+                  
+      return buckets
 
    @spaces.setter
    def spaces(self, value):
@@ -203,7 +299,7 @@ class Typo:
 
    @property
    def bits(self):
-      return list(map(int, map(math.log2, self.spaces)))
+      return list(map(int, map(lambda x : math.log2(x + 1), self.spaces)))
 
    @bits.setter
    def bits(self, bits: int):
@@ -248,3 +344,63 @@ class Typo:
          else:
             bit_values.append(0)
       return bit_values, remaining_bits
+
+
+import language_tool_python
+
+# Initialize the LanguageTool tool
+tool = language_tool_python.LanguageTool('en-US')
+
+
+
+# Test case 1: Correct simple grammar error
+# Define the input sentence
+s1 = "Your the best"
+
+# Use LanguageTool to correct grammar errors
+s2 = tool.correct(s1)
+
+# Check that the output is correct
+print('s1,s2')
+util.show_diff(s2, "You're the best")
+
+# Test case 2: Handle correct input sentence
+# Define the input sentence
+s3 = "You're the best"
+
+# Use LanguageTool to correct grammar errors
+print('s3,s4')
+s4 = tool.correct(s3)
+
+# Check that the output is correct
+util.show_diff(s4, "You're the best")
+
+# Example 1: Correct a complex sentence
+# Define the input sentence
+s5 = "The dog chased it's tail, but its too short."
+
+# Use LanguageTool to correct grammar errors
+s6 = tool.correct(s5)
+
+# Print the corrected sentence
+print('s5,s6')
+util.show_diff(s5,s6)
+print(s6)
+# Expected output: "The dog chased its tail, but it's too short."
+
+# Example 2: Handle correct input sentence
+# Define the input sentence
+s7 = "The quick brown fox jumped over the lazy dog."
+
+# Use LanguageTool to correct grammar errors
+s8 = tool.correct(s7)
+print('s7,s8')
+util.show_diff(s7,s8)
+
+# Print the corrected sentence
+print(s8)
+# Expected output: "The quick brown fox jumped over the lazy dog."
+
+
+
+t = Typo('Hi, How are you?')
---
+++ keyboard/generate_key_rules.py
@@ -0,0 +1,69 @@
+qwerty_neighbors = {
+   'q': 'w',
+   'w': 'qe',
+   'e': 'wr',
+   'r': 'et',
+   't': 'ry',
+   'y': 'tu',
+   'u': 'yi',
+   'i': 'uo',
+   'o': 'ip',
+   'p': 'o',
+   'a': 's',
+   's': 'ad',
+   'd': 'sf',
+   'f': 'dg',
+   'g': 'fh',
+   'h': 'gj',
+   'j': 'hk',
+   'k': 'jl',
+   'l': 'k',
+   'z': 'x',
+   'x': 'zc',
+   'c': 'xv',
+   'v': 'cb',
+   'b': 'vn',
+   'n': 'bm',
+   'm': 'n'
+}
+
+qwerty_neighbors_neighbors = {'q': 'e', 'w': 'r', 'e': 'qt', 'r': 'wy', 't': 'eu', 'y': 'ir', 'u': 'ot', 'i': 'py', 'o': 'u', 'p': 'i', 'a': 'd', 's': 'f', 'd': 'ag', 'f': 'sh', 'g': 'jd', 'h': 'kf', 'j': 'gl', 'k': 'h', 'l': 'j', 'z': 'c', 'x': 'v', 'c': 'zb', 'v': 'xn', 'b': 'cm', 'n': 'v', 'm': 'b'}
+
+def neighbor_neighbor_thats_not_neighbor(c:str,n:str)->str:
+   s1 = qwerty_neighbors_neighbors[c] 
+   s2 = qwerty_neighbors[n]
+   return ''.join(set(s1).intersection(set(s2)))
+
+
+
+rules = {} 
+anti_rules = {} 
+
+for c,ns in qwerty_neighbors.items():
+   for n in ns:
+      cn = '[^\\s'+c+n+']'
+      cnn = '[^\\s'+c+n+neighbor_neighbor_thats_not_neighbor(c,n)+']'
+      
+      rules[f'({cnn*2}){c}({cn*2})'] = f'\\1{n+c}\\2'
+      rules[f'({cn*2}){c}({cnn*2})'] = f'\\1{c+n}\\2'
+      rules[f'({cnn*2}){c}({cnn*2})'] = f'\\1{n}\\2'
+
+for c in qwerty_neighbors.keys():
+   rules[f'((\\s|^)[A-Z]){c}([a-z])'] = f'\\1{c.upper()}\\2'
+
+for c in qwerty_neighbors.keys():
+   rules[f'(^|[^{c}])({c}({c}{c})+)([^{c}]|$)'] = f'\\1\\2{c}\\4'
+
+for c in qwerty_neighbors.keys():
+   anti_rules[f'(^|[^{c}]){c}({c}({c}{c})+)([^{c}]|$)'] = f'\\1\\2{c}\\4'
+
+with open('new.keyboard.rules','w') as f:
+   for k,v in rules.items():
+      f.write(f'{k}\t{v}\n')
+
+with open('anti.keyboard.rules','w') as f:
+   for k,v in anti_rules.items():
+      f.write(f'{k}\t{v}\n')
+
+
+# print(u)
