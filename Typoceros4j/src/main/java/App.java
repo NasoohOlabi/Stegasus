fddadc69c93ea122a4d603a77d4c116760547848
Nasooh Olabi <nasooholabi@gmail.com>
2023-05-22 20:30:56 +0300

    Publish a jar to be able to interact with Typpoceros as a cli

--- Typoceros4j/src/main/java/App.java
+++ Typoceros4j/src/main/java/App.java
@@ -4,25 +4,43 @@ import org.languagetool.language.BritishEnglish;
 import org.languagetool.rules.RuleMatch;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-
-        var s = "hi, how are yiou?";
-        System.out.println(s);
-
-        JLanguageTool langTool = new JLanguageTool(new AmericanEnglish());
-        // comment in to use statistical ngram data:
-        //langTool.activateLanguageModelRules(new File("/data/google-ngram-data"));
-        List<RuleMatch> matches = langTool.check(s);
-        for (RuleMatch match : matches) {
-            System.out.println("Potential error at characters " +
-                    match.getFromPos() + "-" + match.getToPos() + ": " +
-                    match.getMessage());
-            System.out.println("Suggested correction(s): " +
-                    match.getSuggestedReplacements());
+    private static Map<String,Typo> memo = new HashMap<>();
+    private static Typo getTypo(String string) throws IOException {
+        if (memo.containsKey(string)){
+            return memo.get(string);
         }
-
+        Typo typo = new Typo(string);
+        memo.put(string,typo);
+        return typo;
+    }
+    public static void main(String[] args) throws IOException, ValueError {
+        String command;
+        String string;
+        String bytes;
+        Typo typo;
+            Scanner scanner = new Scanner(System.in);
+        while (true) {
+            command = scanner.nextLine();
+            string = scanner.nextLine();
+            if (command.equalsIgnoreCase("encode")){
+            bytes = scanner.nextLine();
+                typo = getTypo(string);
+                var values_remainingBytes = typo.encode_encoder(bytes);
+                var values = values_remainingBytes._1();
+                var remainingBytes = values_remainingBytes._2;
+                System.out.println(typo.encode(values));
+                System.out.println(remainingBytes);
+            } else if (command.equalsIgnoreCase("decode")){
+                var originalText_values = Typo.decode(string,false,null);
+                var originalText = originalText_values._1();
+                var values = originalText_values._2;
+                System.out.println(originalText);
+                System.out.println(values);
+            }
+        }
     }
 }
