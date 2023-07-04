425cdbece68f6232fad9b8c67b3e76ef8fdb9c1d
Nasooh Olabi <nasooholabi@gmail.com>
2023-07-04 19:50:43 +0300

    sample generic dataset

---
+++ SampleData/ConversationsRepo.py
@@ -0,0 +1,23 @@
+import os
+
+import pandas as pd
+
+
+class ConversationsRepo:
+  ConversationsCount = 8628
+  @staticmethod
+  def get(i: int):
+    """gets a chat from the Sample data
+
+    Args:
+        i (int): The number of the conversation [1, 8628]
+    """
+    if i < 1 or i > ConversationsRepo.ConversationsCount:
+      raise ValueError(i)
+    
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_directory, 'topical_chat.csv')
+    
+    df = pd.read_csv(file_path)
+    chats = df.groupby('conversation_id')['message'].agg(list)
+    return chats[i]
