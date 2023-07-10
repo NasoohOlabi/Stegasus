25cb54000c6194c871e6f8afdef4a2fc84fd6016
Nasooh Olabi <nasooholabi@gmail.com>
2023-07-10 11:09:36 +0300

    proper Emojier benchmark

--- Emojier/Emojier.py
+++ Emojier/Emojier.py
@@ -9,6 +9,7 @@ import numpy as np
 import torch
 from icecream import ic
 from scipy.special import softmax  # type: ignore
+from StringSpans import StringSpans
 from transformers import AutoModelForSequenceClassification  # type: ignore
 from transformers import AutoTokenizer  # type: ignore
 from transformers import TFAutoModelForSequenceClassification  # type: ignore
@@ -42,10 +43,8 @@ class Emojier:
   model: Any = None
   tokenizer: Any = None
   multiplicityBits = 1
-  TopFPercent = 0.2
-  @staticmethod
-  def setVerbose( v: bool):
-    verbose = v
+  TopFPercent = 0.15
+  verbose = False
   @staticmethod
   def predict( text: str):
     inputs = Emojier.tokenizer(text, return_tensors="pt")
@@ -74,52 +73,50 @@ class Emojier:
     ranking = np.argsort(scores)
     ranking = ranking.squeeze()[::-1]
     emojis = [Emojier.model.config.id2label[i] for i in ranking]
-    return [emo for emo, score in zip(emojis, sorted_scores) if emo != '🇺🇸' and score > Emojier.TopFPercent]
-  
+    emoticons = [emo for emo, score in zip(emojis, sorted_scores) if emo != '🇺🇸' and score > Emojier.TopFPercent]
+    return Emojier.addMultiplicities(emoticons)
   @staticmethod
-  def encode(text:str,bytes_str:str,verbose=False):
+  def addMultiplicities(emoticons: List[str]):
+    new_emoticons = []
+    for emo in emoticons:
+      for i in range(1,Emojier.multiplicityBits+2):
+        new_emoticons.append(emo * i)
+    return new_emoticons
+  @staticmethod
+  def encode(text:str,bytes_str:str):
     mask = MaskGen(text)
     encoded_so_far = ''
-    ticks = [text[:v] for u,v in mask.NVA_words]
+    ss = StringSpans(text)
+    ticks = [(text[:v],(u,v)) for u,v in mask.NVA_words if (u,v) in ss.words]
     original_length = len(text)
-    new_ending = lambda x : (len(text) - original_length) + len(x)
-    for pre_text in ticks:
-      if verbose:
-        print('-'*20 + 'tick' + '-'*20)
+    curr_offset = lambda : (len(text) - original_length)
+    new_ending = lambda x : curr_offset() + len(x)
+    for pre_text, (u,v) in ticks:
+      u, v = (u + curr_offset(), v+ curr_offset())
       breakPoint = new_ending(pre_text)
       pre_text = text[:breakPoint]
+      Emojier.log('E>'+'-'*20 + 'tick' + '-'*20 + pre_text)
       emoji_options = gaussian_order(Emojier._predict(text[:breakPoint]))
       if len(emoji_options) < 2:
-        if verbose:
-          print(f'pre_text={pre_text},not enough options={emoji_options}')
+        Emojier.log('E>'+f'word={text[u:v]},range={(0,breakPoint)},not enough options={emoji_options}')
         continue
       if bytes_str[0] == "0":
-        if verbose:
-          print(f'pre_text={pre_text},zero start={bytes_str[:5]}')
+        Emojier.log('E>'+f'word={text[u:v]},range={(0,breakPoint)},zero start={bytes_str[:5]}')
         encoded_so_far += bytes_str[0]
         bytes_str = bytes_str[1:]
         continue
       encoded_so_far += bytes_str[0]
       bytes_str = bytes_str[1:] # discard the one
-      if verbose:
-        print(f"word: {pre_text} \nlen: {len(emoji_options)} \temoji_options: {emoji_options}")
       bits = floor(log2(len(emoji_options)))
       taken_bits = bytes_str[:bits]
       ind = int(taken_bits, 2)
       encoded_so_far += bytes_str[:bits]
       bytes_str = bytes_str[bits:]
-      emojis = emoji_options[ind]
-      # Mutliplicity
-      taken_bits = bytes_str[:Emojier.multiplicityBits]
-      mult = int(taken_bits, 2)+1
-      encoded_so_far += bytes_str[:Emojier.multiplicityBits]
-      bytes_str = bytes_str[Emojier.multiplicityBits:]
-      if verbose:
-        print(f'encoded_so_far={encoded_so_far}')
-      if len(emojis) > 0:
-        text = f'{text[0:breakPoint]} {mult * emojis}{text[breakPoint:]}'
-      if verbose:
-        print(f'>>>encoding {taken_bits} = {ind} as {emojis}\nencoded text={text}')
+      emoji = emoji_options[ind]
+      Emojier.log('E>'+f"word={text[u:v]},range={(0,breakPoint)},len({emoji})={len(emoji)},{len(emoji_options)}=len({emoji_options})")
+      Emojier.log('E>'+f'encoded_so_far={encoded_so_far}')
+      if len(emoji) > 0:
+        text = f'{text[0:breakPoint]} {emoji}{text[breakPoint:]}'
     return text, bytes_str
   @staticmethod
   def int_to_binary_string(n: int, length: int) -> str:
@@ -129,13 +126,19 @@ class Emojier:
   @staticmethod
   def cntPrefix(string:str, prefix:str):
     for i in range(4,0,-1):
-      # if verbose:
-      #   print(f"string={string[:len(prefix*i)]},prefix*i={prefix*i},string.startswith(prefix * i)={string.startswith(prefix * i)}",end='|')
+    #   Emojier.log(f"string={string[:len(prefix*i)]},prefix*i={prefix*i},string.startswith(prefix * i)={string.startswith(prefix * i)}",end='|')
       if string.startswith(prefix * i):
-        # print('')
+        # Emojier.log('')
         return i
-    # print('')
+    # Emojier.log('')
     return 0
+  @staticmethod
+  def log(string:str):
+    if Emojier.verbose:
+      print(string)
+    with open('Emojier.log','a') as f:
+      f.write(string+'\n') 
+      
   @staticmethod
   def strip(text:str):
     for label in labels:
@@ -144,55 +147,45 @@ class Emojier:
       text = text.replace(label,'')
     return text
   @staticmethod
-  def decode(text:str,verbose=False):
-    if verbose:
-      print("#"*20 + "decoding" + "#"*20)
-    bytes_str:str = ''
+  def decode(encoded_text:str):
+    text = encoded_text
+    for label in labels:
+      text = text.replace(' '+label,'').replace(label,'')
+    clear_text = text
     mask = MaskGen(text)
-    # ticks = [text[:v] for u,v in mask.NVA_words if text[u:v] not in labels]
-    ticks = [text[:v] for u,v in mask.NVA_words if not any((label in text[u:v] for label in labels))]
+    decoded_so_far = ''
+    ss = StringSpans(text)
+    ticks = [(text[:v],(u,v)) for u,v in mask.NVA_words if (u,v) in ss.words]
     original_length = len(text)
-    new_ending = lambda x : (len(text) - original_length) + len(x)
-    emoji_locations = []
-    for pre_text in ticks:
-      if verbose:
-        print('-'*20 + 'tick' + '-'*20)
+    curr_offset = lambda : (len(text) - original_length)
+    new_ending = lambda x : curr_offset() + len(x)
+    for pre_text, (u,v) in ticks:
+      u, v = (u + curr_offset(), v+ curr_offset())
       breakPoint = new_ending(pre_text)
       pre_text = text[:breakPoint]
-      if verbose:
-        print(f'pre_text={pre_text}')
+      Emojier.log('D>'+'-'*20 + 'tick' + '-'*20 + pre_text)
       emoji_options = gaussian_order(Emojier._predict(text[:breakPoint]))
-      emoji = \
-          [label for label in labels if text[breakPoint:].startswith(' '+label)][0] \
-            if any((text[breakPoint:].startswith(' '+label) for label in labels)) \
-              else None
-      if emoji is not None:
-        bytes_str += '1' # emoji exist
-        bits = floor(log2(len(emoji_options)))
-        idx = emoji_options.index(emoji)
-        bytes_str += Emojier.int_to_binary_string(idx,bits)
-        # Multiplicity
-        # if verbose:
-        #   print('counting multiplicity')
-        multi = Emojier.cntPrefix(text[breakPoint+1:],emoji)
-        bytes_str += Emojier.int_to_binary_string(multi-1,Emojier.multiplicityBits)
-        emoji_locations.append((breakPoint, breakPoint + 1 + len(emoji)*multi))
-        if verbose:
-          print(f"word={pre_text},len(emoji_options)={len(emoji_options)},emoji_options={emoji_options},emoji={emoji},len(emoji)={len(emoji)},multi={multi}")
-      else:
-        if len(emoji_options) < 2:
-          if verbose:
-            print(f"nothing encoded emoji_options={emoji_options}")
-        else:
-          if verbose:
-            print(f"no emoji = zero emoji_options={emoji_options}")
-          bytes_str += '0'
-      if verbose:
-        print(f'bytes_str={bytes_str}')
-    # remove emojies
-    original = text
-    for s,e in reversed(emoji_locations):
-      original = original[:s] + original[e:]
-    return original, bytes_str
+      if len(emoji_options) < 2:
+        Emojier.log('D>'+f'word={text[u:v]},range={(0,breakPoint)},not enough options={emoji_options}')
+        continue
+      
+      emoji = None
+      if any((encoded_text[breakPoint:].startswith(' '+label) for label in emoji_options)):
+        emoticons = [label for label in emoji_options if encoded_text[breakPoint:].startswith(' '+label)]
+        emoticons.sort(key= lambda x: len(x),reverse=True)
+        emoji = emoticons[0]
+              
+      if emoji is None:
+        Emojier.log('D>'+f'word={text[u:v]},range={(0,breakPoint)},zero start={text[breakPoint:breakPoint+5]}')
+        decoded_so_far += "0"
+        continue
+      decoded_so_far += "1"
+      bits = floor(log2(len(emoji_options)))
+      idx = emoji_options.index(emoji)
+      decoded_so_far += Emojier.int_to_binary_string(idx,bits)
+      text = f'{text[0:breakPoint]} {emoji}{text[breakPoint:]}'
+      Emojier.log('D>'+f"word={text[u:v]},range={(0,breakPoint)},len({emoji})={len(emoji)},{len(emoji_options)}=len({emoji_options})")
+      Emojier.log('D>'+f'decoded_so_far={decoded_so_far}')
+    return clear_text, decoded_so_far  
   
 Emojier.model, Emojier.tokenizer = load_model()
