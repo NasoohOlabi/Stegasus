ca50c0e30942853aa1ff93afd0cd70e4514699d2
Nasooh Olabi <nasooholabi@gmail.com>
2023-08-04 11:15:04 +0300

    create a Stegasus.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from .Bot import *
 from .Emojier import *
 from .FrustratinglySimpleBert import MaskedStego
 from .SemanticMasking import MaskGen, SemanticPositions
+from .Stegasus import StegasusDecode, StegasusEncode
