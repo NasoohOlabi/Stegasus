0cf9d2c0d796d63b2fce5535f54f555f1d07a790
Nasooh Olabi <nasooholabi@gmail.com>
2023-08-08 02:56:31 +0300

    use latest jar with base 64

--- TypocerosJar.py
+++ TypocerosJar.py
@@ -1,3 +1,4 @@
+import base64
 import os
 import subprocess
 from typing import Tuple
@@ -25,6 +26,7 @@ class JavaJarWrapper:
     self._process = subprocess.Popen([java_executable, "-jar", jar_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
   def encode(self, string, bytes_str):
+    string = JavaJarWrapper.to64(string)
     try:
       if self._process.stdin is None:
         raise Exception("Error stdin is not available")
@@ -38,12 +40,30 @@ class JavaJarWrapper:
       output = self._process.stdout.readline().decode("utf-8").strip()
       remaining_bytes = self._process.stdout.readline().decode("utf-8").strip()
 
-      return output, remaining_bytes
+      return JavaJarWrapper.from64(output), remaining_bytes
+    except BrokenPipeError:
+      self._start_jar()
+      return self.encode(string, bytes_str)
+  def echo(self, string, bytes_str):
+    string = JavaJarWrapper.to64(string)
+    try:
+      if self._process.stdin is None:
+        raise Exception("Error stdin is not available")
+      if self._process.stdout is None:
+        raise Exception("Error stdout is not available")
+      self._process.stdin.write(b"echo\n")
+      self._process.stdin.write(string.encode("utf-8") + b"\n")
+      self._process.stdin.flush()
+
+      output = self._process.stdout.readline().decode("utf-8").strip()
+
+      return JavaJarWrapper.from64(output)
     except BrokenPipeError:
       self._start_jar()
       return self.encode(string, bytes_str)
     
   def decode(self, string) -> Tuple[str,str]:
+    string = JavaJarWrapper.to64(string)
     try:
       if self._process.stdin is None:
         raise Exception("Error stdin is not available")
@@ -56,13 +76,23 @@ class JavaJarWrapper:
       output = self._process.stdout.readline().decode("utf-8").strip()
       values = self._process.stdout.readline().decode("utf-8").strip()
 
-      return output, values
+      return JavaJarWrapper.from64(output), values
     except BrokenPipeError:
       self._start_jar()
       return self.decode(string)
+  
+  @staticmethod
+  def to64(s:str):
+    b = base64.b64encode(bytes(s, 'utf-8')) # bytes
+    return b.decode('utf-8') # convert bytes to string
+  @staticmethod
+  def from64(s: str):
+      b = base64.b64decode(bytes(s, 'utf-8'))
+      return b.decode('utf-8')
+
     
 if __name__ == '__main__':
   Typo = JavaJarWrapper()
-  r = Typo.encode("hi, how are you?","010101101010001011010")
+  r = Typo.echo("It's been interesting 😎 seeing a project 🙂 with a junior backend starting after a few 👍🏻👍🏻 months here.","010101101010001011010")
 
   print(r)
